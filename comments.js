25ddc84a3d294c74b6faca1ff0cac87d38ffd500
Marcin Kaczmarek <marcin_kaczmarek@interia.eu>
2019-05-28 17:54:08 +0200

    fixes file

--- comments.js
+++ comments.js
@@ -11,43 +11,29 @@ function comments(state = [], action) {
                 id: action.id,
                 text: action.text,
                 votes: 0
-            }
-                , ...state];
+            }, ...state];
+
         case REMOVE_COMMENT:
-            return Object.assign({}, state, {
-                comments: state.comments.filter(comment => comment.id !== action.id)
-            });
-        case EDIT_COMMENT:
             return [{
-                id: action.id,
-                text: action.text,
-                votes: state.comments.filter(comment => comment.id === action.id)
-            }
-                , ...state];
+                comments: state.filter(comment => comment.id !== action.id)
+            }];
+
+        case EDIT_COMMENT:
+            return state.map(comment => {
+                if (comment.id !== action.id) return comment
+                else return { ...comment, text: action.text }
+            })
 
         case THUMB_UP_COMMENT:
-            return [{
-                id: action.id,
-                text: state.comments.map(comment => comment.id === action.id),
-                votes: action.value++
-            }
-                , ...state];
+            return state.map(comment => {
+                if (comment.id !== action.id) return comment
+                else return { ...comment, votes: comment.votes + 1 }
+            })
 
         case THUMB_DOWN_COMMENT:
-            return [{
-                id: action.id,
-                text: state.comments.map(comment => comment.id === action.id),
-                votes: action.value--
-            }
-                , ...state];
+            return state.map(comment => {
+                if (comment.id !== action.id) return comment
+                else return { ...comment, votes: comment.votes - 1 }
+            })
     }
-}
-
-
-
-
-
-
-
-
-
+};
